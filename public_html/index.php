aa23d6daf5da2422793be4397396d01331c9b2e4
Chicao <chicao@chicao.fritz.box>
2018-01-25 21:32:25 +0100

    Added env files capability

--- public_html/index.php
+++ public_html/index.php
@@ -1,8 +1,26 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Dotenv\Dotenv;
+
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../vendor/autoload.php';
+(new Dotenv())->load(__DIR__.'/../.env');
+
+$request = Request::createFromGlobals();
+
+$content = $request->getContent();
 
 $kernel = new Kernel('dev');
 
-echo $kernel->getEnv();
+$apiKeyLive = getenv('SMS_API_KEY_LIVE');
+$apiKeyTest = getenv('SMS_API_KEY_TEST');
+
+$response = new JsonResponse(
+    [
+        '$apiKeyLive' => $apiKeyLive,
+        '$apiKeyTest' => $apiKeyTest,
+    ]
+);
+$response->send();
