508ae5f69f33cb84363a119f2d6455bde1bf07dd
Ademir Francisco da Silva <ademir.francisco.silva@gmail.com>
2018-01-28 02:02:23 +0100

    Added more tests

--- test/Resources/RequestConverterTest.php
+++ test/Resources/RequestConverterTest.php
@@ -29,10 +29,59 @@ class RequestConverterTest extends TestCase
 
     /**
      * @expectedException App\Exception\BadRequestException
+     * @expectedExceptionMessage Invalid json object
      */
     public function testConvertFails()
     {
         $jsonObject = 'invalid json object';
+        $request = new Request([], [], [], [], [], [], $jsonObject);
+
+        $this->requestConverter->convert($request);
+    }
+
+    /**
+     * @expectedException App\Exception\BadRequestException
+     * @expectedExceptionMessage 'recipient' field is required
+     */
+    public function testConvertFailsWithoutRecipient()
+    {
+        $jsonObject = '{"originator":"MessageBird","message":"This is a test message."}';
+        $request = new Request([], [], [], [], [], [], $jsonObject);
+
+        $this->requestConverter->convert($request);
+    }
+
+    /**
+     * @expectedException App\Exception\BadRequestException
+     * @expectedExceptionMessage 'originator' field is required
+     */
+    public function testConvertFailsWithoutOriginator()
+    {
+        $jsonObject = '{"recipient":"31612345678","message":"This is a test message."}';
+        $request = new Request([], [], [], [], [], [], $jsonObject);
+
+        $this->requestConverter->convert($request);
+    }
+
+    /**
+     * @expectedException App\Exception\BadRequestException
+     * @expectedExceptionMessage 'message' field is required
+     */
+    public function testConvertFailsWithoutMessage()
+    {
+        $jsonObject = '{"recipient":31612345678,"originator":"MessageBird"}';
+        $request = new Request([], [], [], [], [], [], $jsonObject);
+
+        $this->requestConverter->convert($request);
+    }
+
+    /**
+     * @expectedException App\Exception\BadRequestException
+     * @expectedExceptionMessage Invalid originator - the maximum length is 11 characters
+     */
+    public function testConvertFailsWithInvalidOriginator()
+    {
+        $jsonObject = '{"recipient":31612345678,"originator":"MessageBirdSMSMessage","message":"This is a test message."}';
         $request = new Request([], [], [], [], [], [], $jsonObject);
 
         $this->requestConverter->convert($request);
